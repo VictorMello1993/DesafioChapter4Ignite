155c3f3fdf844b8653dfa524289b98c7b7583c47
Victor Mello <victorsmello93@gmail.com>
2022-02-01 23:02:10 -0300

    implementação do teste unitário de transfer

--- src/modules/statements/useCases/transferBetweenAccounts/TransferBetweenAccountsUseCase.spec.ts
+++ src/modules/statements/useCases/transferBetweenAccounts/TransferBetweenAccountsUseCase.spec.ts
@@ -1,4 +1,22 @@
-// describe('Transfer between accounts', () => {
-//   it('It should be able to transfer between accounts', async () => {
-//   })
-// })
+import { InMemoryStatementsRepository } from "../../../statements/repositories/in-memory/InMemoryStatementsRepository"
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository"
+import { CreateUserUseCase } from "../../../users/useCases/createUser//CreateUserUseCase";
+import { TransferBetweenAccountsUseCase } from "../transferBetweenAccounts/TransferBetweenAccountsUseCase";
+
+let usersRepositoryInMemory: InMemoryUsersRepository;
+let statementsRepositoryInMemory: InMemoryStatementsRepository;
+let transferBetweenAccountsUseCase: TransferBetweenAccountsUseCase;
+let createUserUseCase: CreateUserUseCase;
+
+describe('Transfer between accounts', () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new InMemoryUsersRepository();
+    statementsRepositoryInMemory = new InMemoryStatementsRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    transferBetweenAccountsUseCase = new TransferBetweenAccountsUseCase(usersRepositoryInMemory, statementsRepositoryInMemory);
+  })
+
+  it('It should be able to transfer between accounts', async () => {
+    //Será implementado depois.
+  })
+})
